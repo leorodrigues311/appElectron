5dfcaf44d2414ab2e16538855401fbd917b95d9e
Leonardo Rodrigues <49464793+leorodrigues311@users.noreply.github.com>
2023-10-18 16:01:51 -0300

    try catch

--- db.js
+++ db.js
@@ -3,6 +3,39 @@ import {chaveAPI, chaveApp, database, portDatabase, baseUrl} from "./connect.js"
 const pg = require ("pg")
 
 
+const btnSalvaConfig = document.getElementById('closeModalButton');
+btnSalvaConfig.addEventListener("click", alteraChaveApp)
+btnSalvaConfig.addEventListener("click", criaTabela)
+
+window.addEventListener("load", criaTabela)
+
+async function criaTabela() {
+    const Client = require ("pg").Client
+    const cliente = new pg.Client({
+        user: "postgres",
+        password: "inova@613188#",
+        host: "127.0.0.1",
+        port: portDatabase,
+        database: database
+        })
+
+    try{
+        cliente.connect()
+        await cliente.query("CREATE TABLE configlojaintegrada (chaveid integer NOT NULL, chaveapi character varying(255), chaveapp character varying(255), CONSTRAINT configlojaintegrada_pk PRIMARY KEY (chaveid)) WITH (OIDS=FALSE);")
+        await cliente.query("INSERT INTO configlojaintegrada(chaveid) VALUES (1);")
+        this.alert("Tabela criada com sucesso")
+
+    } catch(e) {
+        let r = await cliente.query("select chaveid from configlojaintegrada")
+        .then(results => {
+            let resultado = results.rows
+            resultado = JSON.parse(JSON.stringify(resultado))
+            return resultado
+        })
+
+            }
+}
+
  //prametros de conexão com o banco de dados inova passados abaixo
 
 async function connectDb(){
@@ -27,7 +60,6 @@ async function connectDb(){
 
     })
 
-
     //finaliza a conexão com o banco de dados depois de rodar a query
     .finally(() => cliente.end())
     return r
@@ -50,13 +82,9 @@ function alteraChaveApp(){
         })
 
     cliente.connect()
-    console.log(typeof(database),database,typeof(portDatabase),portDatabase)
     cliente.query(text, values)
     
     .finally(() => cliente.end())
 }
-const btnSalvaConfig = document.getElementById('closeModalButton');
-btnSalvaConfig.addEventListener("click", alteraChaveApp)
-
 
 export default connectDb()
