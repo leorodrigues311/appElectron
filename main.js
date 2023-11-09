10022091b4bdfd088ba5c0e97576b55b3800edd2
Leonardo Rodrigues <49464793+leorodrigues311@users.noreply.github.com>
2023-11-09 12:06:04 -0300

    Adição do icone e builder

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-
-
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
+const path = require('path');
 
 let mainWindow;
+let appTray = null;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
-        width:800,
+        width: 800,
         height: 800,
+        show: false, // Inicia a janela como oculta
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -16,4 +17,41 @@ app.on('ready', () => {
 
     // Carrega a URL corretamente
     mainWindow.loadFile(`index.html`);
+
+    mainWindow.on('close', (event) => {
+        if (!app.isQuitting) {
+            event.preventDefault();
+            mainWindow.hide();
+        }
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+
+    const iconName = path.join(__dirname, 'tray-icon.png');
+    appTray = new Tray(iconName);
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Abrir Aplicativo',
+            click: function () {
+                mainWindow.show();
+            }
+        },
+        {
+            label: 'Fechar Aplicativo',
+            click: function () {
+                app.isQuitting = true;
+                app.quit();
+            }
+        }
+    ]);
+    appTray.setToolTip('Seu aplicativo está em execução.');
+    appTray.setContextMenu(contextMenu);
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
